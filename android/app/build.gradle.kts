849b4050e67a1d8d6aaae499a73162f1deb2fe08
ssk <developer.kss@gmail.com>
2025-08-08 11:33:40 +0900

    com.ssk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_ui_playground"
+    namespace = "com.ssk.flutter_ui_playground"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_ui_playground"
+        applicationId = "com.ssk.flutter_ui_playground"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/flutter_ui_playground/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_ui_playground/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_ui_playground
+package com.ssk.flutter_ui_playground
 
 import io.flutter.embedding.android.FlutterActivity
 
